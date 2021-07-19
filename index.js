abea73e83729a416493e7b1b0afa6162a28b2e49
Ryan Johnson <ryanleejwebdev@gmail.com>
2021-07-19 09:54:20 -0500

    Make get request end points for product reviews and offers

--- index.js
+++ index.js
@@ -16,4 +16,52 @@ app.get('/', (req, res) => {
   res.send('Welcome to the Amazon Scraper! 🥳')
 })
 
+// GET product details
+app.get('/products/:productId', async (req, res) => {
+  // "dynamic placeholder?" - 'yep, :productId is what you're looking for'
+  const { productId } = req.params
+
+  try {
+    // git info for a specific product
+    const response = await request(`${baseUrl}&url=https://www.amazon.com/dp/${productId}`)
+    // dp = product details
+    res.json(JSON.parse(response)) // get the json data string from the response and parse it into a usable JS object
+  } catch (error) {
+    // console.error(error)
+    res.json(error)
+  }
+})
+
+// GET product reviews
+app.get('/products/:productId/reviews', async (req, res) => {
+  // "dynamic placeholder?" - 'yep, :productId is what you're looking for'
+  const { productId } = req.params
+
+  try {
+    // git info for a specific product
+    const response = await request(`${baseUrl}&url=https://www.amazon.com/product-reviews/${productId}`)
+    // dp = product details
+    res.json(JSON.parse(response)) // get the json data string from the response and parse it into a usable JS object
+  } catch (error) {
+    // console.error(error)
+    res.json(error)
+  }
+})
+
+// GET product offers
+app.get('/products/:productId/offers', async (req, res) => {
+  // "dynamic placeholder?" - 'yep, :productId is what you're looking for'
+  const { productId } = req.params
+
+  try {
+    // git info for a specific product
+    const response = await request(`${baseUrl}&url=https://www.amazon.com/gp/offer-listing/${productId}`)
+    // dp = product details
+    res.json(JSON.parse(response)) // get the json data string from the response and parse it into a usable JS object
+  } catch (error) {
+    // console.error(error)
+    res.json(error)
+  }
+})
+
 app.listen(PORT, () => console.log(`Server is paying attention on port ${PORT}`)) // "Pay attention!"
