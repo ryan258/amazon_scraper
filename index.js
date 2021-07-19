33f9deaeabb4ec623778381c77c94b4eb3f0341a
Ryan Johnson <ryanleejwebdev@gmail.com>
2021-07-19 08:06:18 -0500

    Initialize Amazon scrapper

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const request = require('request-promise')
+
+const app = express()
+const PORT = process.env.PORT || 5000
+
+app.use(express.json()) // "hey express, parse JSON input"
+
+app.get('/', (req, res) => {
+  // "here's that 1 route you need, you express app w/ the callback you usually need"
+  // "now respond with the following when someone visits the '/' route"
+  res.send('Welcome to the Amazon Scraper! 🥳')
+})
